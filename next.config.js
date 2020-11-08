a89534d3929f58ca291bfc1fbf582c48239613f6
Alex MacCaw <maccman@gmail.com>
2020-11-08 18:15:00 +1300

    Make work offline

--- next.config.js
+++ next.config.js
@@ -1,2 +1,28 @@
 const withNextra = require('nextra')('nextra-theme-docs', './theme.config.js')
-module.exports = withNextra()
+const withOffline = require('next-offline')
+
+const nextConfig = {
+  workboxOpts: {
+    swDest: process.env.NEXT_EXPORT
+      ? 'service-worker.js'
+      : 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /\.(png|woff2|svg)(\?.*)?$/,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'staticOfflineCache',
+        }
+      },
+      {
+        urlPattern: /^https?.*/,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'revalidateOfflineCache',
+        },
+      },
+    ],
+  },
+}
+
+module.exports = withOffline(withNextra(nextConfig))
