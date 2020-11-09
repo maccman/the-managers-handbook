3a188546edc4e36cc691c01113711714d418988b
Alex MacCaw <maccman@gmail.com>
2020-11-09 16:02:22 +1300

    Add chapter one

--- components/callout.js
+++ components/callout.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 // https://www.notion.so/Callout-blocks-5b2638247b54447eb2e21145f97194b0
 const Callout = ({ children, background = 'bg-gray-300 dark:text-gray-800', emoji = '💡' }) => {
-  return <div className={`${background} flex rounded-lg callout mt-6 py-2`}>
-    <div className="pl-3 pr-2 py-2 select-none text-xl">{emoji}</div>
-    <div className="pr-4 py-2 -mt-6 leading-6">{children}</div>
+  return <div className={`${background} flex rounded-lg callout mt-6 py-3`}>
+    <div className="pl-4 pr-3 py-2 select-none text-xl">{emoji}</div>
+    <div className="pr-5 py-2 -mt-6 leading-6">{children}</div>
   </div>
 }
 
